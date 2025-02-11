ef9c1f6b12599b7480c9d6e79fbab2180296b020
onjsdnjs <onjsdnjs@gmail.com>
2025-02-11 21:21:25 +0900

    lecture

--- src/main/java/io/servlet/servlet/RegisterFormServlet.java
+++ src/main/java/io/servlet/servlet/RegisterFormServlet.java
@@ -13,8 +13,7 @@ import java.io.PrintWriter;
 public class RegisterFormServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         response.setContentType("text/html; charset=UTF-8");
         PrintWriter out = response.getWriter();
